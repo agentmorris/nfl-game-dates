5cfcaf1cb4f0b252872c5c624859262e193d7b13
Dan Morris <dan@microsoft.com>
2021-08-29 19:58:08 -0700

    initialization

---
+++ README.md
@@ -0,0 +1,65 @@
+## nfl-game-dates
+
+Python script (and hopefully soon a Web app) to retrieve a chronologically sorted list of NFL games for any week, historical or scheduled, from pro-football-reference.com.
+
+### But why?
+
+I am critically dependent on NFL football for my physical fitness.  So dependent that I wrote an [excessively long blog post](http://rockicon.net/wp/2019/10/22/the-doctor-of-rocks-tfip-total-football-immersion-program/) about how hard I work to avoid football scores so I'm motivated to exercise every day.  During the season, of course, that's football that was just played the prior weekend.  But even during the offseason, I watch old playoff games that I don't really remember.  Either way, I <i>need</i> to avoid knowing the outcomes of football games.
+
+With the old NFL Game Pass Web site, this was easy: games were listed in order, so I could watch early games without fear of finding out the outcome of late games.  As of 8/2021, with a recent major revision to their site, this is no longer the case.  Ergo, before watching a week of new football, or even an old playoff week, I need to know the order in which games were played.
+
+If you're wondering "but don't all the scores tick along the bottom of the screen?"... yes, they do, which is why I use a [nice little program](https://aka.ms/scoreblocker) that [Neel](https://www.microsoft.com/en-us/research/people/neel/) wrote to put a big black rectangle on the bottom of the screen.  Very sophisticated technology.  Sidebar: sports are more fun without the distracting ticker anyway.
+
+If you're wondering "but don't they go to game breaks and tell you about other games?"... yes, they do, but <b>I'm a f'ing pro</b>, so I always know when a game break is coming and I can get my headphones out and my eyes averted in plenty of time.  Because I'm a pro.
+
+If you're wondering "but don't you talk to other people about football?"... no, no I do not.
+
+
+### Usage
+
+`python nfl-game-dates.py [year] [week]`
+
+"year" is the start of the season, not the calendar year of the game.  I.e., 2012 week 17 is in 2013.
+
+"week" is the week to retrieve.  Can be:
+
+* A 1-indexed number from 1 to 22, where 22 would be the Super Bowl after the start of the 17-game season
+* A playoff round name, from ["wild card","divisional","championship","super bowl"]
+
+
+### Examples
+
+#### Using a playoff round name
+```
+python nfl-game-dates.py 2012 divisional
+
+Parsing games...
+Baltimore Ravens at Denver Broncos, 2013-01-12 16:36:00
+Green Bay Packers at San Francisco 49ers, 2013-01-12 20:25:00
+Seattle Seahawks at Atlanta Falcons, 2013-01-13 13:05:00
+Houston Texans at New England Patriots, 2013-01-13 16:40:00
+```
+
+#### Future games
+```
+python nfl-game-dates.py 2021 1
+
+Parsing games...
+Dallas Cowboys at Tampa Bay Buccaneers, 2021-09-09 20:20:00
+Pittsburgh Steelers at Buffalo Bills, 2021-09-12 13:00:00
+Philadelphia Eagles at Atlanta Falcons, 2021-09-12 13:00:00
+Los Angeles Chargers at Washington Football Team, 2021-09-12 13:00:00
+Arizona Cardinals at Tennessee Titans, 2021-09-12 13:00:00
+Jacksonville Jaguars at Houston Texans, 2021-09-12 13:00:00
+San Francisco 49ers at Detroit Lions, 2021-09-12 13:00:00
+Seattle Seahawks at Indianapolis Colts, 2021-09-12 13:00:00
+Minnesota Vikings at Cincinnati Bengals, 2021-09-12 13:00:00
+New York Jets at Carolina Panthers, 2021-09-12 13:00:00
+Denver Broncos at New York Giants, 2021-09-12 16:25:00
+Miami Dolphins at New England Patriots, 2021-09-12 16:25:00
+Green Bay Packers at New Orleans Saints, 2021-09-12 16:25:00
+Cleveland Browns at Kansas City Chiefs, 2021-09-12 16:25:00
+Chicago Bears at Los Angeles Rams, 2021-09-12 20:20:00
+Baltimore Ravens at Las Vegas Raiders, 2021-09-13 20:15:00
+```
+
---
+++ nfl-game-dates.py
@@ -0,0 +1,342 @@
+#
+# nfl-game-dates.py
+#
+# Core functions for retrieving game dates and times from pro-football-reference.com
+#
+# The main function is load_game_times(year,week), which returns the list of game teams
+# and start times for every game that week.  Though I haven't tested every combination, it
+# attempts to handle different season lengths and the addition of the wild card week. 
+#
+# Pulls data from pro-football-reference.com.
+#
+
+#%% Imports and constants
+
+import re
+import requests
+from dateutil import parser as dateparser
+from bs4 import BeautifulSoup
+
+base_url = 'https://www.pro-football-reference.com'
+
+playoff_round_names = ['wildcard','divisional','championship','superbowl']
+
+
+#%% Classes
+
+class GameInfo:
+    def __init__(self,teamAway,teamHome,start_time):
+        self.teamAway = teamAway
+        self.teamHome = teamHome
+        self.start_time = start_time
+    
+    def __str__(self):
+        return '{} at {}, {}'.format(self.teamAway,self.teamHome,self.start_time)
+    
+    def __repr__(self):
+        return '{} at {}, {}'.format(self.teamAway,self.teamHome,self.start_time)
+        
+        
+#%% Functions
+    
+def get_number_of_weeks_in_season(year):
+    """
+    Return the number of weeks in any regular season >= 1961
+    """    
+    # See https://en.wikipedia.org/wiki/NFL_regular_season
+    assert isinstance(year,int)
+    assert year >= 1961
+    if year <= 1977:
+        if year == 1966:
+            return 15
+        else:
+            return 14
+    elif year <= 1989:
+        if year == 1982:
+            return 17
+        # Technically unnecessary, but this season actually had fewer games than
+        # other seasons, and we may need to factor that in later, so this is a reminder
+        # that it's different than, e.g., 1988
+        elif year == 1987:
+            return 16
+        else:
+            return 16
+    elif year <= 2020:
+        if year == 1993 or year == 2001:
+            return 18
+        else:
+            return 17
+    else:
+        assert year >= 2021
+        return 18
+
+
+def normalize_string(s):
+    """
+    Lowercase and remove whitespace from *s*
+    """
+    assert isinstance(s,str)
+    return s.lower().strip().replace(' ','')
+
+
+def playoff_round_to_offset(round_name,year):
+    """
+    Given a playoff round name from playoff_round_names, return the number of games
+    after the end of the regular season that this round took place.  E.g. for 2010,
+    "wildcard" is 1 week after the end of the regular season
+    """
+    
+    round_name = normalize_string(round_name)
+    assert round_name in playoff_round_names
+    if year < 1978:
+        assert round_name != 'wildcard', "The Wild Card round didn't start until 1978"
+        round_mapping = {'divisional':1,'championship':2,'superbowl':3}        
+    else:
+        round_mapping = {'wildcard':1,'divisional':2,'championship':3,'superbowl':4}
+    return round_mapping[round_name]
+                         
+    
+def is_super_bowl(week,year):
+    assert isinstance(week,int) and isinstance(year,int)
+    return week == get_number_of_weeks_in_season(year) + playoff_round_to_offset('superbowl',year)
+        
+
+def load_game_times_from_url(url,week,year):
+    """
+    See load_game_times() for return values
+    """
+    
+    assert isinstance(week,int)
+    assert isinstance(year,int)
+    
+    html_text = requests.get(url).text
+    week_soup = BeautifulSoup(html_text,'html.parser')
+    game_tables = week_soup.find_all('table', {'class':'teams'})
+    
+    if len(game_tables) == 0:
+        # Corner case: for some years, the super bowl is inexplicably considered
+        # a third game in the championship round.
+        if is_super_bowl(week,year):
+            print('Warning: no games found for {} week {}, reverting to week {}'.format(
+                year,week,week-1))
+            url = url.replace(str(week),str(week-1))
+            games = load_game_times_from_url(url,week,year)
+            return [games[-1]]
+        else:
+            raise ValueError('Could not parse any games from {}'.format(url))
+    
+    # This is what a game table looks like:
+    """
+    	<table class="teams">
+		<tbody>
+		<tr class="date"><td colspan=3>Sep 8, 2011</td></tr>		       
+		<tr class="loser">
+			<td><a href="/teams/nor/2011.htm">New Orleans Saints</a></td>
+			<td class="right">34</td>
+			<td class="right gamelink">
+				<a href="/boxscores/201109080gnb.htm">F<span class="no_mobile">inal</span></a>
+				
+			</td>
+		</tr>
+		<tr class="winner">
+			<td><a href="/teams/gnb/2011.htm">Green Bay Packers</a></td>
+			<td class="right">42</td>
+			<td class="right">&nbsp;
+			</td>
+		</tr>
+		</tbody>
+	</table>
+    """
+        
+    games = []
+    
+    print('Parsing games...')
+    
+    # game_table = game_tables[0]
+    for i_game,game_table in enumerate(game_tables):
+        
+        game_links = game_table.find_all('a')
+        boxscore_links = [s for s in game_links if 'boxscores' in str(s)]
+        assert len(boxscore_links) == 1
+        boxscore_link = boxscore_links[0]
+        relative_path = boxscore_link['href']
+        boxscore_url = base_url + relative_path
+        
+        html_text = requests.get(boxscore_url).text
+        game_soup = BeautifulSoup(html_text,'html.parser')
+        
+        # E.g.:
+        #
+        # Regular season:
+        #
+        # New Orleans Saints at Green Bay Packers - September 8th, 2011 | Pro-Football-Reference.com
+        #
+        # Playoff:
+        #
+        # Wild Card - Atlanta Falcons at Arizona Cardinals - January 3rd, 2009 | Pro-Football-Reference.com
+        #
+        # Starting in 2021:
+        # 
+        # Dallas Cowboys  at  Tampa Bay Buccaneers - September 9th, 2021 - Raymond James Stadium | Pro-Football-Reference.com
+        # Cleveland Browns  at  Kansas City Chiefs - September 12th, 2021 - GEHA Field at Arrowhead Stadium | Pro-Football-Reference.com
+        
+        title_raw = game_soup.title.getText()
+        
+        # Super Bowls use 'vs.', all other games use 'at' (maybe London games use 'vs.'?)
+        assert ' at ' in title_raw or ' vs. ' in title_raw
+        team_tokens = re.split(r' at | vs. ',title_raw,maxsplit=1)
+        assert len(team_tokens) == 2
+        teamAway = team_tokens[0].strip()
+        if ' - ' in teamAway:
+            teamAway = teamAway.split(' - ')[1].strip()
+        teamHome = team_tokens[1].strip()
+        if ' - ' in teamHome:
+            teamHome = teamHome.split(' - ')[0].strip()
+        
+        # The scorebox looks like this:
+        """
+        <div class="scorebox_meta">
+		<div>Thursday Sep 8, 2011</div><div><strong>Start Time</strong>: 8:40pm</div><div><strong>Stadium</strong>: <a href="/stadiums/GNB00.htm">Lambeau Field</a> </div><div><strong>Attendance</strong>: <a href="/years/2011/attendance.htm">70,555</a></div><div><strong>Time of Game</strong>: 3:09</div>
+		<div><em>Logos <a href="http://www.sportslogos.net/">via Sports Logos.net</a>
+            / <a href="//www.sports-reference.com/blog/2016/06/redesign-team-and-league-logos-courtesy-sportslogos-net/">About logos</a></em></div>
+        <div>
+        """
+        scoreboxes = game_soup.find_all('div',{'class':'scorebox_meta'})
+        assert len(scoreboxes) == 1
+        scorebox = scoreboxes[0]
+        scorebox_divs = scorebox.find_all('div')
+        date_text = scorebox_divs[0].getText().strip()
+        time_text = scorebox_divs[1].getText()
+        assert time_text.startswith('Start Time')
+        time_text = time_text.split(':',1)[1].strip()
+        
+        datetime_text = date_text + ' ' + time_text
+        
+        # Now we have, e.g.:
+        #
+        # Thursday Sep 8, 2011 8:40pm
+        #        
+        # I'm *almost* positive it's returning games in browser-local time        
+        game_start_time = dateparser.parse(datetime_text)
+        
+        game = GameInfo(teamAway,teamHome,game_start_time)
+        games.append(game)
+    
+    # ...for each game
+    
+    # Sort games
+    sorted_games = sorted(games, key=lambda x: x.start_time)
+    
+    return sorted_games
+    
+def load_game_times(year,week):
+    """
+    Load all games from the specified week, return as a list of Game objects.
+    
+    Year refers to the year of week 1, i.e. the 2012 Super Bowl took place in 2013.
+    
+    Week can be a 1-indexed integer from 1 to 22 (19, 21, or 22 would be the Super Bowl, depending
+    on the year), or it can be a string from playoff_game_names.
+    
+    Only seasons >= 1961 are supported.
+    """
+    
+    if isinstance(year,str):
+        year = int(year)
+    assert isinstance(year,int)
+    assert year >= 1966 and year <= 2050
+    
+    if isinstance(week,str):
+        try:
+            week = int(normalize_string(week))
+        except:
+            pass
+        
+    if isinstance(week,str):
+        round_name = normalize_string(week)
+        assert round_name in playoff_round_names,'Unrecognized week name {}'.format(round_name)
+        number_of_regular_season_weeks = get_number_of_weeks_in_season(year)
+        playoff_offset = playoff_round_to_offset(round_name,year)
+        week = number_of_regular_season_weeks + playoff_offset
+        
+    # One way or another, *week* is an integer now
+    assert isinstance(week,int)
+    
+    url = base_url + '/years/' + str(year) + '/week_' + str(week) + '.htm'
+    # os.startfile(url) 
+    
+    return load_game_times_from_url(url,week,year)
+        
+
+#%% Test driver
+
+if False:
+
+    #%%
+    
+    # https://www.pro-football-reference.com/years/2011/week_1.htm
+    year = 2011
+    week = 1        
+    games = load_game_times(year,week)
+    for s in games:
+        print(s)
+    
+    #%%
+    
+    year = '2008'
+    week = 'wild card'
+    games = load_game_times(year,week)
+    for s in games:
+        print(s)
+        
+    #%%
+    
+    year = '1991'
+    week = 'sUpeR      boWL'
+    games = load_game_times(year,week)
+    for s in games:
+        print(s)
+        
+    #%%
+    
+    year = 2002
+    week = 'super bowl'
+    games = load_game_times(year,week)
+    for s in games:
+        print(s)
+        
+    #%%
+    
+    year = 2021
+    week = 1
+    games = load_game_times(year,week)
+    for s in games:
+        print(s)
+    
+#%% Command-line driver
+
+import argparse
+import sys
+
+def main():
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'year',
+        help='Year of the season start (i.e., year of week 1, not the calendar year of the game)')
+    parser.add_argument(
+        'week',
+        help='Week to fetch, either a number (1-22) or a playoff week name (wild card, divisional, championship, super bowl)'
+        )
+    
+    if len(sys.argv[1:]) == 0:
+        parser.print_help()
+        parser.exit()
+
+    args = parser.parse_args()
+    games = load_game_times(args.year,args.week)
+    for s in games:
+        print(s)
+
+if __name__ == '__main__':
+    main()
